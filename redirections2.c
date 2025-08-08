69e7102603bc7d43ed6506879cbde957925134ff
helkhouj <hichamelkhouji@gmail.com>
2025-08-08 10:31:02 +0100

    Fix: Move redirection functions to redirections2.c to resolve unused function errors

--- redirections2.c
+++ redirections2.c
@@ -3,13 +3,109 @@
 /*                                                        :::      ::::::::   */
 /*   redirections2.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: student <student@student.42.fr>            +#+  +:+       +#+        */
+/*   By: student <student@student@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/15 10:00:00 by student           #+#    #+#             */
 /*   Updated: 2024/01/15 10:00:00 by student          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "minishell.h"
+
+static int	open_input_file(char *filename)
+{
+	int	fd;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		print_error(filename, strerror(errno));
+		return (-1);
+	}
+	return (fd);
+}
+
+static int	open_output_file(char *filename, int append)
+{
+	int	fd;
+	int	flags;
+
+	flags = O_WRONLY | O_CREAT;
+	if (append)
+		flags |= O_APPEND;
+	else
+		flags |= O_TRUNC;
+	fd = open(filename, flags, 0644);
+	if (fd == -1)
+	{
+		print_error(filename, strerror(errno));
+		return (-1);
+	}
+	return (fd);
+}
+
+int	handle_heredoc(char *delimiter)
+{
+	int		pipe_fd[2];
+	char	*line;
+
+	if (pipe(pipe_fd) == -1)
+		return (-1);
+	while (1)
+	{
+		line = readline("> ");
+		if (!line)
+			break ;
+		if (ft_strcmp(line, delimiter) == 0)
+		{
+			free(line);
+			break ;
+		}
+		write(pipe_fd[1], line, ft_strlen(line));
+		write(pipe_fd[1], "\n", 1);
+		free(line);
+	}
+	close(pipe_fd[1]);
+	return (pipe_fd[0]);
+}
+
+static int	setup_input_redirection(char *file)
+{
+	int	fd;
+
+	fd = open_input_file(file);
+	if (fd == -1)
+		return (-1);
+	if (dup2(fd, STDIN_FILENO) == -1)
+		return (close(fd), -1);
+	close(fd);
+	return (0);
+}
+
+static int	setup_output_redirection(char *file, int append)
+{
+	int	fd;
+
+	fd = open_output_file(file, append);
+	if (fd == -1)
+		return (-1);
+	if (dup2(fd, STDOUT_FILENO) == -1)
+		return (close(fd), -1);
+	close(fd);
+	return (0);
+}
+
+static int	setup_heredoc_redirection(char *file)
+{
+	int	fd;
+
+	fd = handle_heredoc(file);
+	if (fd == -1)
+		return (-1);
+	if (dup2(fd, STDIN_FILENO) == -1)
+		return (close(fd), -1);
+	close(fd);
+	return (0);
+}
 
 int	setup_redirections(t_redir *redirs)
 {
